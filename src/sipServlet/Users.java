fddb13fa6690065cac8b7e06c607e5f91eeb8a73
Ruben <r.agussglz@gmail.com>
2025-11-19 09:50:35 +0100

    Initial commit

---
+++ src/sipServlet/Users.java
@@ -0,0 +1,28 @@
+package sipServlet;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "users")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Users {
+	@XmlElement(name = "user")
+	private List<User> listUsers;
+
+	public List<User> getListUsers() {
+		return listUsers;
+	}
+
+	public void setListUsers(List<User> listUsers) {
+		this.listUsers = listUsers;
+	}
+
+	@Override
+	public String toString() {
+		return "Users [listUsers=" + listUsers + "]";
+	}
+	
+}
