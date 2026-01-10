b955cc9d2456166c0b7caf646985a684bde0a6ff
Ruben <r.agussglz@gmail.com>
2026-01-10 19:08:55 +0100

    chore: adapt arguments to specifications

--- src/Proxy.java
+++ src/Proxy.java
@@ -1,18 +1,36 @@
 import proxy.ProxyUserLayer;
 
 public class Proxy {
-	private static final boolean DEBUG = Boolean.getBoolean("debug");
 	public static void main(String[] args) throws Exception {
-		if (DEBUG) System.out.println("Proxy launching with args: " + String.join(", ", args));
 		if (args.length < 1) {
-			System.err.println("Usage: java Proxy <listenPort> [loose]");
+			System.err.println("Usage: java Proxy <listenPort> [<loose-routing> <debug>]");
 			return;
 		}
+		
 		int listenPort = Integer.parseInt(args[0]);
-		boolean loose = args.length > 1 && ("loose".equalsIgnoreCase(args[1]) || "true".equalsIgnoreCase(args[1]));
-		boolean force = args.length > 2 && ("force".equalsIgnoreCase(args[2]) || "true".equalsIgnoreCase(args[2]));
+		boolean loose = false;
+		if (args.length >= 2) {
+		    loose = "true".equalsIgnoreCase(args[1]) || "loose".equalsIgnoreCase(args[1]);
+		}
+		
+		boolean debug = false;
+		if (args.length >= 3) {
+		    debug = "true".equalsIgnoreCase(args[2]);
+		} else {
+             debug = Boolean.getBoolean("debug");
+        }
+        
+        // Hidden argument for testing force loose routing if needed
+        boolean force = false;
+        if (args.length >= 4) {
+            force = "true".equalsIgnoreCase(args[3]);
+        }
+
+		if (debug) System.out.println("Proxy launching with args: " + String.join(", ", args));
+
 		ProxyUserLayer userLayer = new ProxyUserLayer(listenPort, loose, force);
-		userLayer.setDebug(DEBUG);
+		userLayer.setDebug(debug);
 		userLayer.startListening();
 	}
 }
+
--- src/UA.java
+++ src/UA.java
@@ -1,22 +1,33 @@
 import ua.UaUserLayer;
 
 public class UA {
-    private static final boolean DEBUG = Boolean.getBoolean("debug");
     public static void main(String[] args) throws Exception {
-        if (args.length < UAHelp.mandatoryArgs || args.length > UAHelp.mandatoryArgs + UAHelp.optionalArgs) {
+        if (args.length < UAHelp.minArgs || args.length > UAHelp.maxArgs) {
             UAHelp.print_help();
             return;
         }
-        if (DEBUG) System.out.println("UA launching with args: " + String.join(", ", args));
-
+        
         String sip_user = args[0];
         int listenPort = Integer.parseInt(args[1]);
         String proxyAddress = args[2];
         int proxyPort = Integer.parseInt(args[3]);
-        int registerExpires = args.length == 5 ? Integer.parseInt(args[4]) : 3000;
+        
+        boolean debug = false;
+        if (args.length >= 5) {
+             debug = Boolean.parseBoolean(args[4]);
+        } else {
+             debug = Boolean.getBoolean("debug"); // Fallback to system prop if not provided, for backward comp if needed, but per instructions we should look at arg
+        }
+        
+        int registerExpires = 3000;
+        if (args.length >= 6) {
+            registerExpires = Integer.parseInt(args[5]);
+        }
+
+        if (debug) System.out.println("UA launching with args: " + String.join(", ", args));
 
         UaUserLayer userLayer = new UaUserLayer(sip_user, listenPort, proxyAddress, proxyPort, registerExpires);
-        userLayer.setDebug(DEBUG);
+        userLayer.setDebug(debug);
 
         new Thread() {
             @Override
@@ -36,11 +47,12 @@ public class UA {
     }
 
     private static class UAHelp {
-        protected static int mandatoryArgs = 4;
-        protected static int optionalArgs = 1;
+        protected static int minArgs = 4;
+        protected static int maxArgs = 6;
         
         protected static void print_help() {
-            System.out.println("Usage: java UA <sip_user> <listen_port> <proxy_address> <proxy_port> [<register_seconds>]");
+            System.out.println("Usage: java UA <sip_user> <listen_port> <proxy_address> <proxy_port> [<debug> <register_seconds>]");
         }
     }
 }
+
